589b99b927ad8e0dc1681814597db408ce1ea22b
ReturnSweetPork <happylovetkd@naver.com>
2020-01-24 22:28:06 +0900

    create 기능 추가함

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState, useCallback } from 'react';
 import TodoTemplate from './components/TodoTemplate';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
@@ -35,10 +35,26 @@ const App = () => {
       checked: true,
     },
   ]);
+  // 고윳값으로 사용될 아이디임
+  // ref를 사용하여 변수담기
+  const nextId = useRef(7);
+
+  const onInsert = useCallback(
+    text => {
+      const todo = {
+        id: nextId.current,
+        text,
+        checked: false,
+      };
+      setTodos(todos.concat(todo));
+      nextId.current += 1; // nextID에 1씩더해주는 함수임)
+    },
+    [todos],
+  );
 
   return (
     <TodoTemplate>
-      <TodoInsert />
+      <TodoInsert onInsert={onInsert} />
       <TodoList todos={todos} />
     </TodoTemplate>
   );
--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import { MdAdd } from 'react-icons/md';
 import './TodoInsert.scss';
 
-const TodoInsert = () => {
+const TodoInsert = ({ onInsert }) => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback(e => {
+    setValue(e.target.value);
+    console.log(e.target.value);
+  }, []);
+
+  const onSubmit = useCallback(
+    e => {
+      onInsert(value);
+      setValue(''); //value값 초기화과정
+      e.preventDefault();
+    },
+    [onInsert, value],
+  );
+
   return (
-    <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세여" />
+    <form className="TodoInsert" onSubmit={onSubmit}>
+      <input
+        placeholder="할 일을 입력하세여"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
